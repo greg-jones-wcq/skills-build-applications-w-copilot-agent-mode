446a89960c2934ac85ff6aa324b552c4d8ce8da5
greg-jones-wcq <138427401+greg-jones-wcq@users.noreply.github.com>
2026-09-07 05:47:37 +0000

    Step 5a

--- octofit-tracker/backend/src/server.ts
+++ octofit-tracker/backend/src/server.ts
@@ -20,11 +20,11 @@ const baseUrl = codespaceName
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/users', usersRouter);
-app.use('/api/teams', teamsRouter);
-app.use('/api/activities', activitiesRouter);
-app.use('/api/leaderboard', leaderboardRouter);
-app.use('/api/workouts', workoutsRouter);
+app.use('https://${codespaceName}-8000.app.github.dev/api/users', usersRouter);
+app.use('https://${codespaceName}-8000.app.github.dev/api/teams', teamsRouter);
+app.use('https://${codespaceName}-8000.app.github.dev/api/activities', activitiesRouter);
+app.use('https://${codespaceName}-8000.app.github.dev/api/leaderboard', leaderboardRouter);
+app.use('https://${codespaceName}-8000.app.github.dev/api/workouts', workoutsRouter);
 
 app.get('/api/health', (_request, response) => {
   response.json({
